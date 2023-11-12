7f7ade796928e353465dfcfff1025aa1308f4c1d
OswinJTC <ch0005ai@e.ntu.edu.sg>
2023-11-13 01:03:06 +0800

    v1

---
+++ src/main/java/com/tai/DetectiveConanTest/DetectiveConanTestApplication.java
@@ -0,0 +1,13 @@
+package com.tai.DetectiveConanTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectiveConanTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetectiveConanTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tai/DetectiveConanTest/DetectiveConanTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tai.DetectiveConanTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetectiveConanTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
